55ff2623634bf537114f387075b0100c1e349cd9
Abhishek Kapoor <kapoorkk1980@gmail.com>
2025-04-08 10:04:09 +0530

    fix: update FloatingIcons layout for improved positioning and responsiveness

--- components/floatingIcons.tsx
+++ components/floatingIcons.tsx
@@ -4,7 +4,7 @@ import IconComp from './IconComp'
 
 const FloatingIcons = () => {
     return (
-        <div className="fixed top-1/2 -translate-y-1/2 right-0 z-50 flex flex-col items-center justify-center gap-2 p-4">
+        <div className="fixed w-fit bottom-0 left-1/2 md:top-1/2 md:bottom-auto md:left-auto md:right-0 -translate-x-1/2 md:translate-x-0 md:-translate-y-1/2 z-50 flex flex-row md:flex-col items-center justify-center gap-2 p-2">
             <IconComp href="https://wa.me/14155238886" Icon={IconBrandWhatsappFilled} />
             <IconComp href="https://instagram.com" Icon={IconBrandInstagram} />
             <IconComp href="https://facebook.com" Icon={IconBrandFacebookFilled} />
